2db1f86f4c3c9a553da09d50600e691afc8b4481
Manuel Barzi <manuelbarzi@Manuels-MacBook-Pro.local>
2016-04-04 00:06:55 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,5 @@
+## XClassJS
+
+#### An eXtension on simple Class inheritance in JavaScript, from John Resig.
+
+##### Usage: TODO
---
+++ src/main.js
@@ -0,0 +1,14 @@
+(function() {
+	'use strict';
+
+	js.run(function() {
+
+		var content = document.getElementById('content');
+
+		easy.center(content);
+
+		easy.visible(true, content);
+
+	});
+
+})();
