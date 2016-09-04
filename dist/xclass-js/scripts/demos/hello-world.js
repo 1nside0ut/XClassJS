36c8b77122ff1c9a012f5a0873d3484b880a1aa4
Manuel Barzi <manuelbarzi@Manuels-MacBook-Pro.local>
2016-09-04 14:37:37 +0200

    update npm config

--- dist/xclass-js/scripts/demos/hello-world.js
+++ dist/xclass-js/scripts/demos/hello-world.js
@@ -1,32 +1,32 @@
 (function() {
 
-	console.log('// hello world');
+    console.log('// hello world');
 
-	/**
-	 * HelloWorld class.
-	 */
-	var HelloWorld = Class.extend({
+    /**
+     * HelloWorld class.
+     */
+    var HelloWorld = Class.extend({
 
-		init : function HelloWorld(salutation) {
-			this._salutation = salutation || 'Hello World!';
-		},
+        init: function HelloWorld(salutation) {
+            this._salutation = salutation || 'Hello World!';
+        },
 
-		salute : function() {
-			console.log(this._salutation);
-		}
+        salute: function() {
+            console.log(this._salutation);
+        }
 
-	});
+    });
 
-	// use HelloWorld class
+    // use HelloWorld class
 
-	var hello = new HelloWorld();
+    var hello = new HelloWorld();
 
-	hello.salute(); // outputs 'Hello World!'
+    hello.salute(); // outputs 'Hello World!'
 
-	// pass a new salutation
+    // pass a new salutation
 
-	hello = new HelloWorld('Hola Mundo!');
+    hello = new HelloWorld('Hola Mundo!');
 
-	hello.salute(); // outputs 'Hola Mundo!'
+    hello.salute(); // outputs 'Hola Mundo!'
 
 })();
